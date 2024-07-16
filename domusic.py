630a8d9970a136857ebaa75b9621fae15ad506aa
dome320 <129560188+dome320@users.noreply.github.com>
2024-07-16 10:59:52 -0400

    Update domusic.py

--- domusic.py
+++ domusic.py
@@ -160,7 +160,35 @@ def load_cds_csv(filepath):
                     # Handle the case where conversion to integer fails
                     continue
     note_strings.append("0") 
-    return note_strings    
+    return note_strings
+
+def generate_dataset(directory, output_file, n=2):
+    # Put notes in csv files into a list 
+    all_notes = []
+    for file in os.listdir(directory):
+        filepath = os.path.join(directory, file)
+        notes = load_cds_csv(filepath)
+        all_notes.extend(notes)
+
+    # Make list into a dataset 
+    dataset = []
+    for i in range(n, len(all_notes)):
+        features = all_notes[i-n:i]
+        label = all_notes[i]
+        dataset.append(features + [label])
+    
+    # Make dataset into csv file
+    
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Write header
+        header = [f'note_t-{i}' for i in range(n, 0, -1)] + ['label']
+        writer.writerow(header)
+        # Write data
+        writer.writerows(dataset)
+
+generate_dataset("CSD/english/csv", "note_sequences.csv",2)
+
 
 def load_cds_csv_numbers(filepath):
     note_strings = []
